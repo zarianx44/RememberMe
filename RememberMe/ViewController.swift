d7ba4d921319a137cec476d01560513bb994bdc6
zarianx44 <zchau3@gmail.com>
2025-04-28 12:44:01 -0400

    Initial Commit

---
+++ RememberMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RememberMe
+//
+//  Created by Zara on 2025-04-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RememberMeTests/RememberMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeTests.swift
+//  RememberMeTests
+//
+//  Created by Zara on 2025-04-25.
+//
+
+import Testing
+@testable import RememberMe
+
+struct RememberMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
